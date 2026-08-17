6f86ac622fc15104d192586713ecc9c3a49af526
Sarthak Gupta <sarthakgupta@umass.edu>
2026-08-17 12:55:52 -0400

    Update expense handling in tracker.py
    Refactor expense input and total calculation logic.

--- tracker.py
+++ tracker.py
@@ -1,10 +1,17 @@
 items=int(input("Enter the numnber of items you would like to include in the list"))
 i=0
 total=0.00
+expenses=[]
+
 
 while(i<items):
-  expense=float(input("Enter expense: "))
-  total=total+expense
+  expenses.append(float(input("Enter expense: ")))
+  i+=1
+
+i=0
+while(i<items):
+  total+=expenses[i]
   i+=1
   
 print(f"total is {total:.2f}")
+  
